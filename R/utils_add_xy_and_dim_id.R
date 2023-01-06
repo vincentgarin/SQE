e96cadad0ab2b81d95afc69fe054a7c369a95694
vgarin <vincent.garin6@gmail.com>
2023-01-06 11:53:47 +0530

    Change in the plot of the QTL effect to identify the non tested parent x background configuration

--- R/utils_add_xy_and_dim_id.R
+++ R/utils_add_xy_and_dim_id.R
@@ -22,6 +22,16 @@ add_xy_and_dim_id <- function(d){
   d$y <- pop_env_lk[as.character(d[, 1])]
   d$x <- p_lk[as.character(d[, 2])]
 
+  # complement the grid of points
+  ref_grid <- expand.grid(1:max(p_lk), 1:max(pop_env_lk))
+  ref_grid$pt <- paste0(ref_grid$Var1, '_', ref_grid$Var2)
+  d$pt <- paste0(d$x, '_', d$y)
+
+  d <- merge(x = ref_grid, y = d, by = 'pt', all.x = TRUE)
+  d <- d[, -c(1, 9, 10)]
+  colnames(d)[1:2] <- c('x', 'y')
+  d$not_tested <- FALSE
+  d$not_tested[is.na(d$par)] <- TRUE
 
   # Reduce the parent names to plot
   p_un <- as.character(p_un)
--- R/utils_plot_Qmain_QxE.R
+++ R/utils_plot_Qmain_QxE.R
@@ -11,10 +11,12 @@ plot_Qmain_QxE <- function(d, pop_env_un, par_un, var_unit, main, text.size = 18
   face.y <- rep('plain', length(pop_env_un))
   face.y[grep(pattern = '_main', x = pop_env_un)] <- 'bold'
 
-  p <- ggplot(d) +
-    geom_point(aes(x=x, y=y, fill = effect, size = log10pval), shape = 21) +
+  p <- ggplot(d, aes(x=x, y=y, fill = effect)) +
+    geom_point(aes(size = log10pval, shape='tested')) +
+    geom_point(data=subset(d, not_tested), aes(shape='not tested'), size=3) +
     scale_fill_gradient2(low = "red", mid = "white", high = "blue",
                          guide = guide_colorbar(order = 1), name = var_unit) +
+    scale_shape_manual(values=c('tested'=21, 'not tested'=4), breaks = c('tested', 'not tested'), name = 'status') +
     scale_y_continuous(breaks = 1:length(pop_env_un), labels = rev(pop_env_un)) +
     scale_x_continuous(breaks = 1:length(par_un), labels = as.character(par_un),
                        position = "top") +
